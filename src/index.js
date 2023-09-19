43743060dde147ad9f74cf1c311db46787cc082e
ShivaBansfore-JGEC <shivabansfore123@gmail.com>
2023-09-19 08:10:59 +0530

    feat: added a setup for email using nodemailer

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const { sendBasicEmail } = require('./services/email-service');
 const { PORT } = require('./config/serverConfig');
 
 const setupAndStartServer = async () => {
@@ -11,6 +12,12 @@ const setupAndStartServer = async () => {
     app.use(bodyParser.urlencoded({extended: true}));
     app.listen(PORT, () => {
         console.log(`Server started on port no: ${PORT}`);
+        sendBasicEmail(
+            'support@admin.com',
+            'developer.shiva777@gmail.com',
+            'This is a testing email',
+            'hey how are you i hope you like the service'
+        )
     })
 }
 
